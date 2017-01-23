268f0aaa39a3ad51663e3f69ec2216764f408e39
WS-PC08\ws1 <k_fujino@workscience.jp>
2017-01-23 11:08:53 +0900

    first commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	// speedを制御する
+	public float speed = 10;
+	//public float x = 0;//Input.GetAxis("Horizontal");
+	//public float z = 0;//Input.GetAxis("Vertical");
+	public void SpeedUp(){
+		speed++;
+	}
+
+	public void SpeedDown(){
+		speed--;
+	}
+
+	/*public void TrunLeft(){
+		x--;
+	}
+
+	public void TrunRight(){
+		x++;
+	}*/
+
+	public void FixedUpdate ()
+	{
+		float x =  Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+		 
+		Rigidbody rigidbody = GameObject.Find("RowBoat").GetComponent<Rigidbody>();
+
+		rigidbody.AddForce( x * speed , 0, z * speed);
+
+	}
+
+
+
+
+
+}
